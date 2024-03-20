1a1fc7956749b3c5689edfa396951d961bb6d1d1
Wisit018 <lipton_pink@hotmail.co.th>
2024-03-20 14:57:29 +0700

    Finish :100:

--- react-login-api/src/Register.js
+++ react-login-api/src/Register.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import ReactDOM from "react-dom/client";
+
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
@@ -8,8 +8,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
+
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
